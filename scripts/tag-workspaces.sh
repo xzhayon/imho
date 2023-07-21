ea8ec2f9bd6b1693155f70cd31717a6d1f7ce8aa
Alberto Villa <alberto.villa@facile.it>
2023-07-21 21:05:54 +0000

    chore: add error messages to tagging script

--- scripts/tag-workspaces.sh
+++ scripts/tag-workspaces.sh
@@ -12,17 +12,17 @@ done
 shift "$((${OPTIND} - 1))"
 
 WORKSPACES="$@"
-test "${WORKSPACES}" = "" && exit 1
+test "${WORKSPACES}" = "" && echo Workspace list cannot be empty >&2 && exit 1
 
 for workspace in ${WORKSPACES}; do
     test "${VERBOSE}" = "1" && echo "workspace: $(realpath ${workspace})"
 
     name="$(sed -En 's/.*"name": "(.+)".*/\1/p' ${workspace}/${MANIFEST} | head -1)"
-    test "${name}" = "" && exit 1
+    test "${name}" = "" && echo "Cannot parse name for workspace \"${workspace}\"" >&2 && exit 1
     test "${VERBOSE}" = "1" && echo "name: ${name}"
 
     version="$(sed -En 's/.*"version": "(.+)".*/\1/p' ${workspace}/${MANIFEST} | head -1)"
-    test "${version}" = "" && exit 1
+    test "${version}" = "" && echo "Cannot parse version for workspace \"${workspace}\"" >&2 && exit 1
     test "${VERBOSE}" = "1" && echo "version: ${version}"
 
     test "${DRYRUN}" = "1" &&
