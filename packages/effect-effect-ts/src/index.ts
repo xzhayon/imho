f2b80574c0bb5abfa5d97f5ab20f8862e26de6c4
Alberto Villa <alberto.villa@facile.it>
2023-07-24 20:11:38 +0000

    feat(effect): make Effect-TS IO synchronous
    BREAKING CHANGES: `IO`-derived effects need to be wrapped to be used in `Effect` ecosystem

--- packages/effect-effect-ts/src/index.ts
+++ packages/effect-effect-ts/src/index.ts
@@ -1,4 +1,4 @@
-import { IO, UIO } from '@effect-ts/core/Effect'
+import { Effect, IO } from '@effect-ts/core'
 import { Either } from '@effect-ts/core/Either'
 import { Option } from '@effect-ts/core/Option'
 import * as _ from '@imho/effect'
@@ -7,10 +7,12 @@ export type EffectTs<A> = A extends _.Option<infer B>
   ? Option<EffectTs<B>>
   : A extends _.Either<infer E, infer B>
   ? Either<EffectTs<E>, EffectTs<B>>
-  : A extends _.IO<infer B> | _.Task<infer B>
+  : A extends _.IO<infer B>
+  ? IO.IO<EffectTs<B>>
+  : A extends _.Task<infer B>
   ? B extends _.Either<infer E, infer C>
-    ? IO<EffectTs<E>, EffectTs<C>>
-    : UIO<EffectTs<B>>
+    ? Effect.IO<EffectTs<E>, EffectTs<C>>
+    : Effect.UIO<EffectTs<B>>
   : A extends Error
   ? A
   : A extends (...args: any) => any
