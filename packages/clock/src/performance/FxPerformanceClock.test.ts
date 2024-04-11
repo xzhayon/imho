f9122bd0d179cb2fa84c33612d0704c789b7f4b5
Alberto Villa <villa.alberto@gmail.com>
2024-04-11 15:28:47 +0200

    fix: update Fx dependency (#29)

--- packages/clock/src/performance/FxPerformanceClock.test.ts
+++ packages/clock/src/performance/FxPerformanceClock.test.ts
@@ -5,12 +5,10 @@ import { FxPerformanceClock } from './FxPerformanceClock'
 describe('FxPerformanceClock', () => {
   describe('now', () => {
     test('returning current timestamp', async () => {
-      function* f() {
-        return (yield* perform(Clock.now())).valueOf()
-      }
-
       await expect(
-        run(f(), layer().with(Clock, FxPerformanceClock())),
+        run(function* () {
+          return (yield* perform(Clock.now())).valueOf()
+        }, layer().with(Clock, FxPerformanceClock())),
       ).resolves.toBeCloseTo(performance.timeOrigin + performance.now(), -1)
     })
   })
