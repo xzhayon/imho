80a0087fd7961f5c0564622d7ccac7e73466c918
Alberto Villa <alberto.villa@facile.it>
2024-04-23 22:17:47 +0000

    feat(codec): add Fx decoder

--- packages/codec/src/Codec.ts
+++ packages/codec/src/Codec.ts
@@ -1,8 +1,14 @@
-import { Decoder } from './Decoder'
+import { Decoder, FxDecoder } from './Decoder'
 import { Encoder } from './Encoder'
 
 export interface Codec<A, O = A, I = unknown>
   extends Decoder<A, I>,
+    Encoder<A, O> {
+  is(u: unknown): u is A
+}
+
+export interface FxCodec<A, O = A, I = unknown>
+  extends FxDecoder<A, I>,
     Encoder<A, O> {
   is(u: unknown): u is A
 }
--- packages/codec/src/Decoder.ts
+++ packages/codec/src/Decoder.ts
@@ -1,3 +1,10 @@
+import { Effector } from '@xzhayon/fx'
+import { CodecError } from './CodecError'
+
 export interface Decoder<A, I = unknown> {
   decode(i: I): A
+}
+
+export interface FxDecoder<A, I = unknown> {
+  decode(i: I): Effector<never, A, CodecError>
 }
