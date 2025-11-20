d748459a95a1340e568b8abceb0d9ca03a84c73b
Alberto Villa <alberto.villa@facile.it>
2025-11-20 16:25:12 +0100

    fix(logger): invoke specific methods in Affex abstraction

--- packages/logger/src/Logger.ts
+++ packages/logger/src/Logger.ts
@@ -22,76 +22,48 @@ export type FxLogger = Pick<Logger, typeof fx.uri> & {
 
 export const tag = fx.tag<FxLogger>('Logger')
 
-const { log } = fx.access(tag, 'log')
+const {
+  log,
+  debug,
+  info,
+  notice,
+  warning,
+  error: _error,
+  critical,
+  alert,
+  emergency,
+} = fx.access(
+  tag,
+  'log',
+  'debug',
+  'info',
+  'notice',
+  'warning',
+  'error',
+  'critical',
+  'alert',
+  'emergency',
+)
 
 export const Logger: { readonly log: FxLogFunctionWithContext } & {
   readonly [K in Severity]: FxLogFunctionWithoutSeverityWithContext
 } = {
-  log: (
-    severity,
-    messageOrAttributes: any,
-    attributesOrError?: any,
-    error?: Error,
-  ) =>
-    log((f) =>
-      typeof messageOrAttributes === 'string'
-        ? f(severity, messageOrAttributes, attributesOrError, error)
-        : f(severity, messageOrAttributes, attributesOrError),
-    ),
-  debug: (messageOrAttributes: any, attributesOrError?: any, error?: Error) =>
-    log((f) =>
-      typeof messageOrAttributes === 'string'
-        ? f('debug', messageOrAttributes, attributesOrError, error)
-        : f('debug', messageOrAttributes, attributesOrError),
-    ),
-  info: (messageOrAttributes: any, attributesOrError?: any, error?: Error) =>
-    log((f) =>
-      typeof messageOrAttributes === 'string'
-        ? f('info', messageOrAttributes, attributesOrError, error)
-        : f('info', messageOrAttributes, attributesOrError),
-    ),
-  notice: (messageOrAttributes: any, attributesOrError?: any, error?: Error) =>
-    log((f) =>
-      typeof messageOrAttributes === 'string'
-        ? f('notice', messageOrAttributes, attributesOrError, error)
-        : f('notice', messageOrAttributes, attributesOrError),
-    ),
-  warning: (messageOrAttributes: any, attributesOrError?: any, error?: Error) =>
-    log((f) =>
-      typeof messageOrAttributes === 'string'
-        ? f('warning', messageOrAttributes, attributesOrError, error)
-        : f('warning', messageOrAttributes, attributesOrError),
-    ),
-  error: (messageOrAttributes: any, attributesOrError?: any, error?: Error) =>
-    log((f) =>
-      typeof messageOrAttributes === 'string'
-        ? f('error', messageOrAttributes, attributesOrError, error)
-        : f('error', messageOrAttributes, attributesOrError),
-    ),
-  critical: (
-    messageOrAttributes: any,
-    attributesOrError?: any,
-    error?: Error,
-  ) =>
-    log((f) =>
-      typeof messageOrAttributes === 'string'
-        ? f('critical', messageOrAttributes, attributesOrError, error)
-        : f('critical', messageOrAttributes, attributesOrError),
-    ),
-  alert: (messageOrAttributes: any, attributesOrError?: any, error?: Error) =>
-    log((f) =>
-      typeof messageOrAttributes === 'string'
-        ? f('alert', messageOrAttributes, attributesOrError, error)
-        : f('alert', messageOrAttributes, attributesOrError),
-    ),
-  emergency: (
-    messageOrAttributes: any,
-    attributesOrError?: any,
-    error?: Error,
-  ) =>
-    log((f) =>
-      typeof messageOrAttributes === 'string'
-        ? f('emergency', messageOrAttributes, attributesOrError, error)
-        : f('emergency', messageOrAttributes, attributesOrError),
-    ),
+  log: (severity, arg1: any, arg2?: any, error?: Error) =>
+    log((f) => f(severity, arg1, arg2, error)),
+  debug: (arg0: any, arg1?: any, error?: Error) =>
+    debug((f) => f(arg0, arg1, error)),
+  info: (arg0: any, arg1?: any, error?: Error) =>
+    info((f) => f(arg0, arg1, error)),
+  notice: (arg0: any, arg1?: any, error?: Error) =>
+    notice((f) => f(arg0, arg1, error)),
+  warning: (arg0: any, arg1?: any, error?: Error) =>
+    warning((f) => f(arg0, arg1, error)),
+  error: (arg0: any, arg1?: any, error?: Error) =>
+    _error((f) => f(arg0, arg1, error)),
+  critical: (arg0: any, arg1?: any, error?: Error) =>
+    critical((f) => f(arg0, arg1, error)),
+  alert: (arg0: any, arg1?: any, error?: Error) =>
+    alert((f) => f(arg0, arg1, error)),
+  emergency: (arg0: any, arg1?: any, error?: Error) =>
+    emergency((f) => f(arg0, arg1, error)),
 }
