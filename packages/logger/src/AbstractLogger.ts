c232b3e18ca9d791a17502a194df51f3be1fb2f2
Alberto Villa <alberto.villa@facile.it>
2025-11-20 16:22:08 +0100

    refactor(logger): simplify logging method implementations in AbstractLogger for improved readability

--- packages/logger/src/AbstractLogger.ts
+++ packages/logger/src/AbstractLogger.ts
@@ -18,73 +18,49 @@ export abstract class AbstractLogger implements Logger {
     messageOrAttributes: any,
     attributesOrError?: any,
     error?: Error,
-  ) =>
-    typeof messageOrAttributes === 'string'
-      ? this.log('debug', messageOrAttributes, attributesOrError, error)
-      : this.log('debug', messageOrAttributes, attributesOrError)
+  ) => this.log('debug', messageOrAttributes, attributesOrError, error)
 
   readonly info: LogFunctionWithoutSeverity = (
     messageOrAttributes: any,
     attributesOrError?: any,
     error?: Error,
-  ) =>
-    typeof messageOrAttributes === 'string'
-      ? this.log('info', messageOrAttributes, attributesOrError, error)
-      : this.log('info', messageOrAttributes, attributesOrError)
+  ) => this.log('info', messageOrAttributes, attributesOrError, error)
 
   readonly notice: LogFunctionWithoutSeverity = (
     messageOrAttributes: any,
     attributesOrError?: any,
     error?: Error,
-  ) =>
-    typeof messageOrAttributes === 'string'
-      ? this.log('notice', messageOrAttributes, attributesOrError, error)
-      : this.log('notice', messageOrAttributes, attributesOrError)
+  ) => this.log('notice', messageOrAttributes, attributesOrError, error)
 
   readonly warning: LogFunctionWithoutSeverity = (
     messageOrAttributes: any,
     attributesOrError?: any,
     error?: Error,
-  ) =>
-    typeof messageOrAttributes === 'string'
-      ? this.log('warning', messageOrAttributes, attributesOrError, error)
-      : this.log('warning', messageOrAttributes, attributesOrError)
+  ) => this.log('warning', messageOrAttributes, attributesOrError, error)
 
   readonly error: LogFunctionWithoutSeverity = (
     messageOrAttributes: any,
     attributesOrError?: any,
     error?: Error,
-  ) =>
-    typeof messageOrAttributes === 'string'
-      ? this.log('error', messageOrAttributes, attributesOrError, error)
-      : this.log('error', messageOrAttributes, attributesOrError)
+  ) => this.log('error', messageOrAttributes, attributesOrError, error)
 
   readonly critical: LogFunctionWithoutSeverity = (
     messageOrAttributes: any,
     attributesOrError?: any,
     error?: Error,
-  ) =>
-    typeof messageOrAttributes === 'string'
-      ? this.log('critical', messageOrAttributes, attributesOrError, error)
-      : this.log('critical', messageOrAttributes, attributesOrError)
+  ) => this.log('critical', messageOrAttributes, attributesOrError, error)
 
   readonly alert: LogFunctionWithoutSeverity = (
     messageOrAttributes: any,
     attributesOrError?: any,
     error?: Error,
-  ) =>
-    typeof messageOrAttributes === 'string'
-      ? this.log('alert', messageOrAttributes, attributesOrError, error)
-      : this.log('alert', messageOrAttributes, attributesOrError)
+  ) => this.log('alert', messageOrAttributes, attributesOrError, error)
 
   readonly emergency: LogFunctionWithoutSeverity = (
     messageOrAttributes: any,
     attributesOrError?: any,
     error?: Error,
-  ) =>
-    typeof messageOrAttributes === 'string'
-      ? this.log('emergency', messageOrAttributes, attributesOrError, error)
-      : this.log('emergency', messageOrAttributes, attributesOrError)
+  ) => this.log('emergency', messageOrAttributes, attributesOrError, error)
 
   protected abstract readonly _log: (
     severity: Severity,
