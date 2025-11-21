00397e1d706005f94b220fbc6123542a41ecc4db
Alberto Villa <alberto.villa@facile.it>
2025-11-21 15:07:22 +0100

    test(logger): rename logger variable to compositeLogger for clarity in CompositeLogger tests

--- packages/logger/src/composite/CompositeLogger.test.ts
+++ packages/logger/src/composite/CompositeLogger.test.ts
@@ -4,14 +4,14 @@ import { CompositeLogger } from './CompositeLogger'
 describe('CompositeLogger', () => {
   const loggerA = new InMemoryLogger()
   const loggerB = new InMemoryLogger()
-  const logger = new CompositeLogger({ loggers: [loggerA, loggerB] })
+  const compositeLogger = new CompositeLogger({ loggers: [loggerA, loggerB] })
 
   test('logging with all loggers', async () => {
     const message = 'foo'
     const attributes = { foo: 'bar' }
     const error = new Error()
 
-    await logger.debug(message, { attributes, error })
+    await compositeLogger.debug(message, { attributes, error })
     expect(loggerA.records.at(-1)).toMatchObject({ message, attributes, error })
     expect(loggerB.records.at(-1)).toMatchObject({ message, attributes, error })
   })
